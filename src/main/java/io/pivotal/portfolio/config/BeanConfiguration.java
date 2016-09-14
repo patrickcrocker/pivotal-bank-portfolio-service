9d4e551e140796e77a48f8a8b13490ad8a76e3a5
patrickcrocker <patrick.crocker@gmail.com>
2016-09-14 10:05:15 -0500

    commented out restTemplate for Brixton.M3 support

--- src/main/java/io/pivotal/portfolio/config/BeanConfiguration.java
+++ src/main/java/io/pivotal/portfolio/config/BeanConfiguration.java
@@ -1,15 +1,15 @@
 package io.pivotal.portfolio.config;
 
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
+// import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.client.RestTemplate;
 
-@Configuration
+//@Configuration
 public class BeanConfiguration {
-	@LoadBalanced
-    @Bean
-    public RestTemplate loadBalanced() {
-        return new RestTemplate();
-    }
+	// @LoadBalanced
+  //   @Bean
+  //   public RestTemplate loadBalanced() {
+  //       return new RestTemplate();
+  //   }
 }
